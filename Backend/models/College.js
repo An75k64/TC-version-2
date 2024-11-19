9cc426c1994682021910abdcf3d61cd47bf75e2e
Ankit Kumar <89681244+An75k64@users.noreply.github.com>
2024-11-19 16:31:58 +0530

    Update College.js add the location city and duration

--- Backend/models/College.js
+++ Backend/models/College.js
@@ -1,20 +1,27 @@
-// College.js
-
 const mongoose = require("mongoose");
 
 // Define College schema
 const collegeSchema = new mongoose.Schema({
-  polytechnicCourses: [{ type: String }],
-  ugCourses: [{ type: String }],
-  pgCourses: [{ type: String }],
+  polytechnicCourses: [{ type: String, required: true }],
+  ugCourses: [{ type: String, required: true }],
+  pgCourses: [{ type: String, required: true }],
   collegeName: { type: String, required: true },
-  location: { type: String, required: true },
-  studentsStrengthPolytechnic: { type: Number},
+  location: {
+    street: { type: String, required: true },
+    landmark: { type: String },
+    state: { type: String, required: true },
+    city: { type: String, required: true },
+    pincode: { type: String, required: true },
+  },
+  studentsStrengthPolytechnic: { type: Number },
   studentsStrengthUG: { type: Number },
   studentsStrengthPG: { type: Number },
   collegeEmail: { type: String, required: true },
   mobileNumber: { type: String, required: true },
-  placementSeason: { type: String, required: true },
+  placementSeasonDuration: {
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+  },
   upcomingEvents: { type: String },
   partnershipInterests: [{ type: String }],
   createdAt: { type: Date, default: Date.now },
